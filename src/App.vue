f1b26523275b8d50a65d6935463974174b90f384
predrag <predrag.mitrovic@sunzinet.com>
2022-12-02 15:40:41 +0100

    dsssd

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>Peki ovo radi</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {
+  }
+});
+</script>
+
+<style>
+
+</style>
